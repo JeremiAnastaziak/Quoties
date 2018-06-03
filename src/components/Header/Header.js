aede0122e57bba403535477aefc5854299e49044
Jeremi Anastaziak <Jeremi.Anastaziak@gmail.com>
2018-06-03 12:20:22 +0200

    search

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,10 @@ import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
+import TextField from 'material-ui/TextField';
+import Card from 'material-ui/Card';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import SearchIcon from 'material-ui/svg-icons/action/search';
 
 import './Header.css';
 
@@ -21,16 +24,14 @@ const Logged = props => {
             targetOrigin={{ horizontal: 'right', vertical: 'top' }}
             anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
         >
-            <Link to="/quotes">
+            <Link to="/">
                 <MenuItem
                     primaryText="Sign out"
                     onClick={() => {
                         firebase
                             .auth()
                             .signOut()
-                            .catch(error => {
-                                console.error(error);
-                            });
+                            .catch(console.error);
                     }}
                 />
             </Link>
@@ -38,26 +39,61 @@ const Logged = props => {
     );
 };
 
-function Header() {
+const Search = props => {
     return (
-        <div>
-            <AppBar
-                title={
-                    <Link
-                        to="/quotes"
-                        style={{
-                            color: '#fff',
-                            textDecoration: 'none',
-                            fontWeight: '300'
-                        }}
-                    >
-                        Quoties
-                    </Link>
+        <IconButton onClick={props.onClick}>
+            <SearchIcon color="#fff"/>
+        </IconButton>
+
+    )
+}
+
+class Header extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            search: false
+        };
+    }
+    render() {
+        return (
+            <div>
+                <AppBar
+                    title={
+                        <Link
+                            to="/quotes"
+                            style={{
+                                color: '#fff',
+                                textDecoration: 'none',
+                                fontWeight: '300'
+                            }}
+                        >
+                            Quoties
+                        </Link>
+                    }
+                    iconElementRight={
+                        <div>
+                            <Search onClick={() => this.setState({ search: !this.state.search })} />
+                            <Logged />
+                        </div>
+                    }
+                />
+                { this.state.search &&
+                    <Card className="panel">
+                        <TextField
+                            ref={input => (this.input = input)}
+                            onChange={e => this.setState({ searchText: e.target.value })}
+                            onFocus={e => this.setState({ edition: true })}
+                            onBlur={e => this.setState({ edition: false })}
+                            name="search"
+                            placeholder="What do you search for?"
+                            fullWidth
+                        />
+                    </Card>
                 }
-                iconElementRight={<Logged />}
-            />
-        </div>
-    );
+            </div>
+        );
+    }
 }
 
 export default Header;
